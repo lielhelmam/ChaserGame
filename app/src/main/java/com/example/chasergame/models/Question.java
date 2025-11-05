915b9a2aff35542de7306a7fe0ad6a2d132969d5
Student-2024 <student-2024@ashalim.org>
2025-11-05 11:00:53 +0200

    init

---
+++ app/src/main/java/com/example/chasergame/models/Question.java
@@ -0,0 +1,11 @@
+package com.example.chasergame.models;
+
+import java.util.ArrayList;
+
+public class Question {
+    public String rightAnswer;
+    public ArrayList<String> wrongAnswers;
+    public String question;
+
+
+}
---
+++ app/src/main/java/com/example/chasergame/models/User.java
@@ -0,0 +1,12 @@
+package com.example.chasergame.models;
+
+public class User {
+    public String id;
+    public String username;
+    public String password;
+    public String email;
+    public boolean isAdmin;
+
+
+
+}
